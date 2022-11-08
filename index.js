1d06c0c8da943aec079d428797f7001fd0db2865
Fernando Aponte <faponte1230@yahoo.com>
2022-11-08 16:29:12 -0500

    commit tree 7bb61118719436c8ce0bcb3d19afe05fa29950c9

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(){
+    const moreCats = [...cats, 'Broom']
+    return moreCats
+}
+
+function prependCat(){
+    const newMoreCats = ['Arnold', ...cats]
+    return newMoreCats
+}
+
+function removeLastCat(){
+    const slicedLastCat = cats.slice(0,2)
+    return slicedLastCat
+}
+
+function removeFirstCat(){
+    const slicedFirstCat = cats.slice(-2)
+    return slicedFirstCat
+}
